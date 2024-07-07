3c853f9b2aa257e7898b091cf6b6b21905e2c652
kyyesa <127214987+rizkiesa07@users.noreply.github.com>
2024-07-07 16:25:52 +0700

    Delete lib/Models/transaction_with_category.dart

--- lib/Models/transaction_with_category.dart
+++
@@ -1,7 +0,0 @@
-import 'package:siAbank/Models/database.dart';
-
-class TransactionWithCategory {
-  final Transaction transaction;
-  final Category category;
-  TransactionWithCategory(this.transaction, this.category);
-}
